f8246e52dd7bb257fcd06dbf37e8a92053d57821
NSU-Turag <abdullah.turag@northsouth.edu>
2024-12-29 23:10:27 +0600

    header found and added

--- src/config/ExternalServerConfig.js
+++ src/config/ExternalServerConfig.js
@@ -2,6 +2,10 @@ const { EXTERNAL_SERVER_URL } = require('./Env');
 
 const ExternalServerConfig = {
   BASE_URL: EXTERNAL_SERVER_URL,
+  headers: {
+    'Accept': 'application/json',
+    'User-Agent': 'PostmanRuntime/7.29.2',
+},
   TIMEOUT: 10000,
 };
 
