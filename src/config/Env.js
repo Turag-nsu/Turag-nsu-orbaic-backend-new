1dfa21db121ac69fee65155248d5f47113c3e587
NSU-Turag <abdullah.turag@northsouth.edu>
2024-12-29 23:31:40 +0600

    env added

--- src/config/Env.js
+++ src/config/Env.js
@@ -4,4 +4,7 @@ module.exports = {
     MONGO_URI: process.env.MONGODB_URI,
     EXTERNAL_SERVER_URL: "https://app.orbaic.com",
     PORT: process.env.PORT || 3000,
+    LOGIN_REQUEST_BODY: process.env.LOGIN_REQUEST_BODY,
+    LOGIN_METHOD: process.env.LOGIN_METHOD,
+    LOGIN_URL: process.env.LOGIN_URL,
 }
